bfff191b36a235592d361937fab521172e1c572f
darkruby <art@darkruby.co.uk>
2022-11-19 20:55:21 +0000

    2022 refresh

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig(() => {
+  return {
+    server: { port: 3030 },
+    plugins: [react()],
+    test: {
+      globals: true,
+    },
+    build: {
+      outDir: "./docs",
+    },
+  };
+});
